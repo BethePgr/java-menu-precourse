fd257aba34675defc710a4d5e70a6078877c1869
SEC <c3392302@naver.com>
2023-05-23 13:43:31 +0900

    feat: 코치가 싫어하는 메뉴들 입력하는 기능 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,6 +1,7 @@
 package menu.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import menu.domain.Coach;
 
 public class InputView {
 
@@ -10,6 +11,11 @@ public class InputView {
 
     public static String inputCoachNames(){
         System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+        return Console.readLine();
+    }
+
+    public static String inputCoachHateList(Coach coach){
+        System.out.println(coach.getName() + "(이)가 못 먹는 메뉴를 입력해 주세요.");
         return Console.readLine();
     }
 }
