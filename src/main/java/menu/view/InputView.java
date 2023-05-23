2ace9b502403848984d397551a83876c4f2b171f
jongwonpc <c3392302@naver.com>
2023-05-24 01:27:21 +0900

    chore: input의 메서드를 output으로 이동

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -5,10 +5,6 @@ import menu.domain.Coach;
 
 public class InputView {
 
-    public static void startGame(){
-        System.out.println("점심 메뉴 추천을 시작합니다.\n");
-    }
-
     public static String inputCoachNames(){
         System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
         return Console.readLine();
---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,16 @@
+package menu.view;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import menu.domain.Coach;
+import menu.service.MenuService;
+import menu.utils.Converter;
+
+public class OutputView {
+
+    public static void startGame(){
+        System.out.println("점심 메뉴 추천을 시작합니다.\n");
+    }
+
+
+}
