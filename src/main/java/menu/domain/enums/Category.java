10fa8bbb5f9e2d140817cb4141aeceef090cc134
jongwonpc <c3392302@naver.com>
2023-05-24 01:11:11 +0900

    feat: 카테고리 중 랜덤으로 하나 가져오는 기능 구현

--- src/main/java/menu/domain/enums/Category.java
+++ src/main/java/menu/domain/enums/Category.java
@@ -1,11 +1,16 @@
 package menu.domain.enums;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum Category {
-    JAPAN("일식",1),
-    KOREA("한식",2),
-    CHINA("중식",3),
-    ASIA("아시안",4),
-    WESTERN("양식",5);
+    JAPAN("일식", 1),
+    KOREA("한식", 2),
+    CHINA("중식", 3),
+    ASIA("아시안", 4),
+    WESTERN("양식", 5);
 
     private final String type;
     private final int value;
@@ -15,4 +20,13 @@ public enum Category {
         this.value = value;
     }
 
+    public static String selectCategory() {
+        int num = Randoms.pickNumberInRange(1, 5);
+        return Arrays.stream(Category.values()).filter(category -> category.value == num).findAny()
+            .orElseThrow(() -> new IllegalArgumentException("[ERROR] 해당 카테고리는 존재하지 않습니다.")).type;
+    }
+
+    public String getType(){
+        return type;
+    }
 }
