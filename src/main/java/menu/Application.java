cfe0774251938ccde41d977c99c5e61af5583cc4
jongwonpc <c3392302@naver.com>
2023-05-24 03:29:42 +0900

    style: 변경

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,6 +1,9 @@
 package menu;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import menu.controller.MenuController;
+import menu.domain.Menu;
+import menu.domain.MenuRepository;
 import menu.domain.enums.Category;
 import menu.utils.Converter;
 
--- src/main/java/menu/domain/MenuRepository.java
+++ src/main/java/menu/domain/MenuRepository.java
@@ -24,33 +24,68 @@ public class MenuRepository {
     }
 
     private static void setJapanFoods() {
-        for (String menuName : Converter.toListSplitsByComma(japanFoods)) {
-            menuList.add(new Menu(Category.JAPAN, menuName));
-        }
+        menuList.add(new Menu(Category.JAPAN,"규동"));
+        menuList.add(new Menu(Category.JAPAN,"우동"));
+        menuList.add(new Menu(Category.JAPAN,"미소시루"));
+        menuList.add(new Menu(Category.JAPAN,"스시"));
+        menuList.add(new Menu(Category.JAPAN,"가츠동"));
+        menuList.add(new Menu(Category.JAPAN,"오니기리"));
+        menuList.add(new Menu(Category.JAPAN,"하이라이스"));
+        menuList.add(new Menu(Category.JAPAN,"라멘"));
+        menuList.add(new Menu(Category.JAPAN,"오코노미야끼"));
+
+
+
     }
 
     private static void setKoreaFoods() {
-        for (String menuName : Converter.toListSplitsByComma(koreaFoods)) {
-            menuList.add(new Menu(Category.KOREA, menuName));
-        }
-    }
+        menuList.add(new Menu(Category.KOREA,"김밥"));
+        menuList.add(new Menu(Category.KOREA,"김치찌개"));
+        menuList.add(new Menu(Category.KOREA,"쌈밥"));
+        menuList.add(new Menu(Category.KOREA,"된장찌개"));
+        menuList.add(new Menu(Category.KOREA,"비빔밥"));
+        menuList.add(new Menu(Category.KOREA,"칼국수"));
+        menuList.add(new Menu(Category.KOREA,"불고기"));
+        menuList.add(new Menu(Category.KOREA,"떡볶이"));
+        menuList.add(new Menu(Category.KOREA,"제육볶음"));
 
+    }
     private static void setChinaFoods() {
-        for (String menuName : Converter.toListSplitsByComma(chinaFoods)) {
-            menuList.add(new Menu(Category.CHINA, menuName));
-        }
+        menuList.add(new Menu(Category.CHINA, "깐풍기"));
+        menuList.add(new Menu(Category.CHINA, "볶음면"));
+        menuList.add(new Menu(Category.CHINA, "동파육"));
+        menuList.add(new Menu(Category.CHINA, "짜장면"));
+        menuList.add(new Menu(Category.CHINA, "짬뽕"));
+        menuList.add(new Menu(Category.CHINA, "마파두부"));
+        menuList.add(new Menu(Category.CHINA, "탕수육"));
+        menuList.add(new Menu(Category.CHINA, "토마토 달걀볶음"));
+        menuList.add(new Menu(Category.CHINA, "고추잡채"));
     }
 
     private static void setAsiaFoods() {
-        for (String menuName : Converter.toListSplitsByComma(asiaFoods)) {
-            menuList.add(new Menu(Category.ASIA, menuName));
-        }
-    }
+        menuList.add(new Menu(Category.ASIA, "팟타이"));
+        menuList.add(new Menu(Category.ASIA, "카오 팟"));
+        menuList.add(new Menu(Category.ASIA, "나시고렝"));
+        menuList.add(new Menu(Category.ASIA, "파인애플 볶음밥"));
+        menuList.add(new Menu(Category.ASIA, "쌀국수"));
+        menuList.add(new Menu(Category.ASIA, "똠얌꿍"));
+        menuList.add(new Menu(Category.ASIA, "반미"));
+        menuList.add(new Menu(Category.ASIA, "월남쌈"));
+        menuList.add(new Menu(Category.ASIA, "분짜"));
 
+
+    }
     private static void setWesternFoods() {
-        for (String menuName : Converter.toListSplitsByComma(westernFoods)) {
-            menuList.add(new Menu(Category.WESTERN, menuName));
-        }
+        menuList.add(new Menu(Category.WESTERN, "라자냐"));
+        menuList.add(new Menu(Category.WESTERN, "그라탱"));
+        menuList.add(new Menu(Category.WESTERN, "뇨끼"));
+        menuList.add(new Menu(Category.WESTERN, "끼슈"));
+        menuList.add(new Menu(Category.WESTERN, "프렌치 토스트"));
+        menuList.add(new Menu(Category.WESTERN, "바게트"));
+        menuList.add(new Menu(Category.WESTERN, "스파게티"));
+        menuList.add(new Menu(Category.WESTERN, "피자"));
+        menuList.add(new Menu(Category.WESTERN, "파니니"));
+
     }
 
     public static List<String> getOneCategoryOfMenuList(String category) {
@@ -61,7 +96,8 @@ public class MenuRepository {
     }
 
     public static Menu getMenuByName(String name) {
-        return menuList.stream().filter(menu -> menu.getMenuName().equals(name)).findAny().get();
+        return menuList.stream().filter(menu -> menu.getMenuName().equals(name)).findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지않는 메뉴입니다."));
     }
 
     public static List<Menu> getMenuList(){
