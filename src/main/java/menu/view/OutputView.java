fedfca94975b5aaeffaddbc04b81ea70427fbb5c
jongwonpc <c3392302@naver.com>
2023-05-24 01:28:16 +0900

    feat: 코치들의 음식 추천 결과 양식에 맞게 출력하는 기능 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -12,5 +12,19 @@ public class OutputView {
         System.out.println("점심 메뉴 추천을 시작합니다.\n");
     }
 
+    public static void printResult(MenuService menuService){
+        System.out.println("메뉴 추천 결과입니다.\n");
+        System.out.println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]");
+        System.out.println(Converter.makeOneStringByList(menuService.getCategoryList(),"[ 카테고리"));
+        printCoachMenus(menuService);
+    }
 
+    public static void printCoachMenus(MenuService menuService){
+        for(Coach coach : menuService.getCoaches().getCoachList()){
+            List<String> collect = coach.getMyMenuList().getMenuList().stream()
+                .map(menu -> menu.getMenuName()).collect(
+                    Collectors.toList());
+            System.out.println(Converter.makeOneStringByList(collect,"[ " + coach.getName()));
+        }
+    }
 }
