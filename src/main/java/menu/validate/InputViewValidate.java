459955f9f31f471e4ca16868e853d902030cda1d
SEC <c3392302@naver.com>
2023-05-23 12:55:42 +0900

    feat: 구현 조건에 맞는 코치이름을 입력했는지 검증해주는 기능 구현

---
+++ src/main/java/menu/validate/InputViewValidate.java
@@ -0,0 +1,32 @@
+package menu.validate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import menu.utils.Converter;
+
+public class InputViewValidate {
+
+    public static void validateCoachNames(List<String> coachNames){
+        validateAllCoachNamesLength(coachNames);
+        validateCoachesBetween2And5(coachNames);
+    }
+
+    private static void validateCoachNameLengthBetween2And4(String coachName){
+        if(coachName.length() <= 1 || coachName.length() >=5){
+            throw new IllegalArgumentException("[ERROR] 코치의 이름의 길이는 2글자와 4글자 사이여야합니다.");
+        }
+    }
+
+    private static void validateAllCoachNamesLength(List<String> coachNames){
+        for(String coachName : coachNames){
+            validateCoachNameLengthBetween2And4(coachName);
+        }
+    }
+
+    private static void validateCoachesBetween2And5(List<String> coachNames){
+        if(coachNames.size() <=1 || coachNames.size() >=6){
+            throw new IllegalArgumentException("[ERROR] 같이 밥을 먹는 코치의 숫자는 2명과 5명 사이여야 합니다.");
+        }
+    }
+}
